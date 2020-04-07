19dfa4342dd7f55b6d8edfba8087de9920db957d
Richard Ericson <thedrummer_88@hotmail.com>
2020-04-07 08:48:34 +0800

    Added allowed hosts

--- profiles-rest-api/profiles_project/settings.py
+++ profiles-rest-api/profiles_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'u492b61(z0w1)rs!bt)x7-zzbw(zom&5*w1sznte_#u_=d3*7x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-3-20-221-215.us-east-2.compute.amazonaws.com',
+    '127.0.0.1'
+    ]
 
 
 # Application definition
